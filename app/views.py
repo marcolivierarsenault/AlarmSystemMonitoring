884ee0124645d024384962c663139f2dd98fec31
Marc-Olivier Arsenault <marc-olivier.arsenault@ericsson.com>
2016-09-27 21:56:32 -0400

    mostly working, now need docker setup

--- app/views.py
+++ app/views.py
@@ -1,9 +1,16 @@
-from flask import render_template, request, abort, flash, g, redirect, url_for
+from flask import render_template, request, abort, flash, g, redirect, url_for,jsonify
 from app import app
+from config import *
+import requests
 #Default Loader for Views, This class allows the interpretation of the different URL
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
-
+    url = 'https://api.particle.io/v1/devices/'+PARTICLE_DEVICE_ID+'/ArmedStatus'
+    data = dict(arg='joe', access_token=PARTICLE_DEVICE_TOKEN)
+    r = requests.post(url, data=data, allow_redirects=True)
+    if r.json()['return_value'] == 2:
+        return render_template('unarmed.html')
+    else:
+        return render_template('armed.html')
