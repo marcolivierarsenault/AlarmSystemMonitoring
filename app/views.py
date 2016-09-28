e5491e4d11dc45bf3778597c277d23574ce5da1e
Marc-Olivier Arsenault <marcolivier.arsenault@gmail.com>
2016-09-28 15:11:35 -0400

    add webhook for alarm

--- app/views.py
+++ app/views.py
@@ -2,6 +2,8 @@ from flask import render_template, request, abort, flash, g, redirect, url_for,j
 from app import app
 from config import *
 import requests
+import smtplib
+from email.mime.text import MIMEText
 #Default Loader for Views, This class allows the interpretation of the different URL
 
 @app.route('/')
@@ -14,3 +16,14 @@ def index():
         return render_template('unarmed.html')
     else:
         return render_template('armed.html')
+
+@app.route('/alarm')
+def alarm():
+    msg = MIMEText("THE ALARM IS RIGNING")
+    msg['Subject'] = "Alarm at home"
+    msg['From'] = ALARM_EMAIL_FROM
+    msg['To'] = ALARM_EMAIL_TO
+    s = smtplib.SMTP(SMTP_SERVER)
+    s.sendmail(ALARM_EMAIL_FROM, [ALARM_EMAIL_TO], msg.as_string())
+    s.quit()
+    return ""
--- config.py
+++ config.py
@@ -2,3 +2,6 @@ import os
 
 PARTICLE_DEVICE_ID=os.environ["SYSALARM_DEVICE_ID"]
 PARTICLE_DEVICE_TOKEN=os.environ["SYSALARM_API_TOKEN"]
+ALARM_EMAIL_TO="marcolivier.arsenault@gmail.com"
+ALARM_EMAIL_FROM="systemDalarme@gmail.com"
+SMTP_SERVER="172.17.0.1"
