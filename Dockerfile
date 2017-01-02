6adfda53b4eb1d754b8caec251a9b754f504d3d9
Marc-Olivier Arsenault <marcolivier.arsenault@gmail.com>
2017-01-02 13:53:52 -0500

    update server and backend part

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ RUN apt-get update -y
 RUN apt-get install -y python-pip python-dev build-essential
 COPY . /app
 WORKDIR /app
-RUN pip install flask requests
-ENTRYPOINT ["python"]
-CMD ["run.py"]
+RUN pip install flask requests gunicorn
+ENTRYPOINT ["gunicorn", "-b", "0.0.0.0:5000"]
+CMD ["app:app"]
--- systemdalarmefull.ino
+++ systemdalarmefull.ino
@@ -1,27 +1,14 @@
-//Home system to connect to magellan MG5000 
+//Home system to connect to magellan MG5000
 //Report on IFTTT for alarms and is queriable by ifttt to get the value (arm/disarm)
 //Author:Marc-Olivier Arsenault
 //Date:Sept 2016
 
-int stateInPacket = 0; //to move in their bit pattern 
+int stateInPacket = 0; //to move in their bit pattern
 int sysdalarmValue = 2; //to tell IFTTT if system is armed or not
 bool DEBUG = FALSE;
-int values[500] = {0};
-int position = 0;//position in the list
-int lastRead = 0;//position to read in the list
-int lastData = 0;//EPOCH value
-int stringLengt = 0;
-String dataValues = "";
 
 void setup() {
     Serial1.begin(9600);
-    Particle.variable("lastRead",lastRead);
-    Particle.variable("lastData",lastData);
-    Particle.variable("dataValues",dataValues);
-    Particle.variable("stateInPkt", stateInPacket);
-    Particle.variable("lenght", stringLengt);
-    
-    Particle.function("UpdateString", updateString);
     Particle.function("ArmedStatus", getStatus);
     if (DEBUG){
         Serial.begin(9600);
@@ -30,89 +17,78 @@ void setup() {
 
 void loop() {
     if (Serial1.available() > 0) {
-        values[position] = (Serial1.read());
-        position ++;
-        if(position == 500)
-            cleanoutList();
-    }
-    else{
-        if (position > lastRead) {
-            handleData(values[lastRead]);
-            lastRead ++;
-            lastData = Time.now();
-        }
-        
-    }
+        handleData(Serial1.read());
+     }
 }
 
 //Handle all the actual data from the system
 //This method break down each packet the reassamble the frame from the panel
 void handleData(int newValue){
-    if (newValue == 224){ //E0 new start for the paquet 
+    if (newValue == 224){ //E0 new start for the paquet
         stateInPacket = 1;
         if (DEBUG)
             Serial.write(newValue);
     }
     else{
         switch(stateInPacket){
-            
+
             case 1: //represent 20 (the first two digit of the year)
                 if (DEBUG)
                     Serial.write(newValue);
                 if(newValue==20)
                     stateInPacket++;
-                else 
+                else
                     stateInPacket = 0;
                 break;
-            
+
             case 2: //represent 16 (or more) (the last two digit of the year)
                 if (DEBUG)
                     Serial.write(newValue);
                 if(newValue>15)
                     stateInPacket++;
-                else 
+                else
                     stateInPacket = 0;
-                break;  
-            
+                break;
+
             case 3: //represent month of the year 1-12
                 if (DEBUG)
                     Serial.write(newValue);
                 if(newValue==Time.month())
                     stateInPacket++;
-                else 
+                else
                     stateInPacket = 0;
                 break;
-            
+
             case 4: //represent day of the month 1-31
                 if (DEBUG)
                     Serial.write(newValue);
                 if(newValue==Time.day())
                     stateInPacket++;
-                else 
+                else
                     stateInPacket = 0;
                 break;
-                
-            case 5: //represent hour 
+
+            case 5: //represent hour
                 if (DEBUG)
                     Serial.write(newValue);
                 stateInPacket++;
                 break;
-                
-            case 6: //represent minute 
+
+            case 6: //represent minute
                 if (DEBUG)
                     Serial.write(newValue);
                 stateInPacket++;
                 break;
-            
+
             case 7: //Groupe of alarm (tells witch component is used)
                 if (DEBUG)
-                    Serial.write(newValue); 
+                    Serial.write(newValue);
                 if(newValue==2)
                     stateInPacket++;
-                else 
+                else
                     stateInPacket = 0;
                 break;
-            
+
             case 8: //SubGroupe of alarm what is the reason (arm disarm alarm etc.)
                 if (DEBUG)
                     Serial.write(newValue);
@@ -125,16 +101,16 @@ void handleData(int newValue){
                 {
                     stateInPacket++;
                     sendAlarm(); //lance une alarme
-                } 
+                }
                 else if(newValue==11) //Désharmé 11 ou 0x0B
                 {
                     stateInPacket++;
                     sysdalarmValue=2; //Maintenant concidéré comme non armé
                 }
-                else 
+                else
                     stateInPacket = 0;
                 break;
-                
+
             default:
                 stateInPacket = 0;
         }
@@ -147,32 +123,7 @@ int getStatus(String command) {
     return sysdalarmValue;
 }
 
-//method to call when the 
+//method to call when the
 void sendAlarm() {
     Particle.publish("ALARM");
-}
-
-int updateString(String command){
-    dataValues = "";
-    int i;
-    for (i=0;i<position;i++) {
-        dataValues += String(values[i], HEX);
-        dataValues += String(';');
-    }
-    stringLengt = dataValues.length();
-    return 1;
-}
-
-//move the list 400 position ahead (to keep the size of the list small)
-void cleanoutList() {
-    int i;
-    for (i = 0; i < 100; i++) {
-        values[i] = values[i+400];
-    }
-    for (i = 100; i < 500; i++) {
-        values[i] = 0;
-    }
-    lastRead = lastRead - 400;
-    position = position - 400;
-    
 }
