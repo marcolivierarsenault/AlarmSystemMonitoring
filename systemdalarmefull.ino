1505a64f779e59adefce11d3618cb4b7db24a8a8
Marc-Olivier Arsenault <marc-olivier.arsenault@ericsson.com>
2017-01-02 22:15:25 -0500

    fix alarm not always working issue Problem was with the date checking Because Alarm system and electron clocks are not syncronized they had a issue in the evening

--- systemdalarmefull.ino
+++ systemdalarmefull.ino
@@ -5,11 +5,14 @@
 
 int stateInPacket = 0; //to move in their bit pattern
 int sysdalarmValue = 2; //to tell IFTTT if system is armed or not
+char publishString[40];
 bool DEBUG = FALSE;
+bool DEBUG_ONLINE = FALSE;
 
 void setup() {
     Serial1.begin(9600);
     Particle.function("ArmedStatus", getStatus);
+    Particle.function("ConfDebug", changeDebugStatus);
     if (DEBUG){
         Serial.begin(9600);
     }
@@ -53,19 +56,13 @@ void handleData(int newValue){
             case 3: //represent month of the year 1-12
                 if (DEBUG)
                     Serial.write(newValue);
-                if(newValue==Time.month())
-                    stateInPacket++;
-                else
-                    stateInPacket = 0;
+                stateInPacket++;
                 break;
 
             case 4: //represent day of the month 1-31
                 if (DEBUG)
                     Serial.write(newValue);
-                if(newValue==Time.day())
-                    stateInPacket++;
-                else
-                    stateInPacket = 0;
+                stateInPacket++;
                 break;
 
             case 5: //represent hour
@@ -83,6 +80,10 @@ void handleData(int newValue){
             case 7: //Groupe of alarm (tells witch component is used)
                 if (DEBUG)
                     Serial.write(newValue);
+                if (DEBUG_ONLINE){
+                    sprintf(publishString, "%X", newValue); //  see what I did there?
+                    Particle.publish("DEBUG_GROUP", publishString);
+                }
                 if(newValue==2)
                     stateInPacket++;
                 else
@@ -92,6 +93,10 @@ void handleData(int newValue){
             case 8: //SubGroupe of alarm what is the reason (arm disarm alarm etc.)
                 if (DEBUG)
                     Serial.write(newValue);
+                if (DEBUG_ONLINE){
+                    sprintf(publishString, "%X", newValue); //  see what I did there?
+                    Particle.publish("DEBUG_SUBGROUP", publishString);
+                }
                 if(newValue==12) //ARMÉ 12 ou 0x0C
                 {
                     stateInPacket++;
@@ -121,6 +126,15 @@ void handleData(int newValue){
 //1 is armed 2 is unarmed
 int getStatus(String command) {
     return sysdalarmValue;
+}
+
+//Change the setting off the debug Value to remote fix the problem
+int changeDebugStatus(String command) {
+    if(command=="1")
+        DEBUG_ONLINE=TRUE;
+    else
+        DEBUG_ONLINE=FALSE;
+    return DEBUG_ONLINE;
 }
 
 //method to call when the
